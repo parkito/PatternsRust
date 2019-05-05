56eee88e5e027043c397e3aa9bc00f291527a4e6
parkito <artyom-karnov@yandex.ru>
2019-05-05 19:43:52 +0300

    Add task30

---
+++ src/main/kotlin/com/coding/play/tasks/Task30.kt
@@ -0,0 +1,33 @@
+package com.coding.play.tasks
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val bufferSize = scanner.nextInt()
+    val numOfPackages = scanner.nextInt()
+    val packages = ArrayList<Package>(bufferSize)
+    for (i in 0 until numOfPackages) {
+        val str = scanner.next().split(" ")
+        val arrival = str.get(0).toInt()
+        val duration = str.get(1).toInt()
+        packages.add(Package(i, arrival, duration))
+    }
+    processPackages(packages,bufferSize)
+}
+
+fun processPackages(packages: List<Package>, bufferSize: Int) {
+
+}
+
+class Buffer(val size: Int){
+
+}
+
+
+data class Package(
+        val num: Int,
+        val arrival: Int,
+        val duration: Int
+)

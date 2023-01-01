d54614abb3877c49dd4cbd0d1f28b9b55360fa13
parkito <artyom-karnov@yandex.ru>
2022-12-31 20:52:51 -0700

    renew the repo

--- src/main/kotlin/com/coding/play/tasks/Task23.kt
+++
@@ -1,9 +0,0 @@
-package com.coding.play.tasks
-
-fun main(args: Array<String>) {
-    val c1 = scan.next()[0]
-    val c2 = scan.next()[0]
-    print(isCaseEquals(c1, c2))
-}
-
-fun isCaseEquals(a: Char, b: Char): Boolean = (a.isUpperCase() && b.isUpperCase()) || (a.isLowerCase() && b.isLowerCase())
